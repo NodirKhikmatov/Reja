b008b6c610f2441efd2b81921bc69e8878f62134
Nodir8 <Nodirbek8464>
2024-11-23 20:17:32 +0900

    mit task2

--- train.js
+++ train.js
@@ -1,70 +1,70 @@
 // console.log("Jack Ma maslahatlari");
 
-const list = [
-  "yaxshi talaba bo'ling", //0-20
-  "to'gri boshliq tanlang va ko'proq hato qiling", //20-30
-  "uzingizga ishlashingizni boshlang", //30-40
-  "siz kuchli bolgan narsalarni qiling", //40-50
-  "yoshlarga investitsiya qiling", //50-60
-  "endi dam oling, foydasi yoq endi", //60
-];
+// const list = [
+//   "yaxshi talaba bo'ling", //0-20
+//   "to'gri boshliq tanlang va ko'proq hato qiling", //20-30
+//   "uzingizga ishlashingizni boshlang", //30-40
+//   "siz kuchli bolgan narsalarni qiling", //40-50
+//   "yoshlarga investitsiya qiling", //50-60
+//   "endi dam oling, foydasi yoq endi", //60
+// ];
 
 //Callback
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a <= 20 && a <= 30) callback(null, list[1]);
-  else if (a <= 30 && a <= 40) callback(null, list[2]);
-  else if (a <= 40 && a <= 50) callback(null, list[3]);
-  else if (a <= 50 && a <= 60) callback(null, list[4]);
-  else {
-    // setTimeout(() => {
-    //   callback(null, list[5]);
-    // }, 5000);
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a <= 20 && a <= 30) callback(null, list[1]);
+//   else if (a <= 30 && a <= 40) callback(null, list[2]);
+//   else if (a <= 40 && a <= 50) callback(null, list[3]);
+//   else if (a <= 50 && a <= 60) callback(null, list[4]);
+//   else {
+// setTimeout(() => {
+//   callback(null, list[5]);
+// }, 5000);
 
-    setInterval(() => {
-      callback(null, list[5]);
-    }, 1000);
-  }
-}
+//     setInterval(() => {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
 
 // console.log("passed her 0");
-maslahatBering(55, (err, data) => {
-  if (err) console.log("Error", err);
-  else {
-    console.log("javob", data);
-  }
-});
-console.log("passed here 1");
+// maslahatBering(55, (err, data) => {
+//   if (err) console.log("Error", err);
+//   else {
+//     console.log("javob", data);
+//   }
+// });
+// console.log("passed here 1");
 
 //async function ========
 
-async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("insert a number");
-  else if (a <= 20) return list[0];
-  else if (a <= 20 && a <= 30) return list[1];
-  else if (a <= 30 && a <= 40) return list[2];
-  else if (a <= 40 && a <= 50) return list[3];
-  else if (a <= 50 && a <= 60) return list[4];
-  else {
-    // return list[5];
-    // setTimeout(() => {
-    //   callback(null, list[5]);
-    // }, 5000);
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a <= 20 && a <= 30) return list[1];
+//   else if (a <= 30 && a <= 40) return list[2];
+//   else if (a <= 40 && a <= 50) return list[3];
+//   else if (a <= 50 && a <= 60) return list[4];
+//   else {
+//     // return list[5];
+//     // setTimeout(() => {
+//     //   callback(null, list[5]);
+//     // }, 5000);
 
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(list[5]);
-      }, 5000);
-      //===========
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//       //===========
 
-      setInterval(() => {
-        resolve([list5]);
-      }, 4000);
-    });
-  }
-}
+//       setInterval(() => {
+//         resolve([list5]);
+//       }, 4000);
+//     });
+//   }
+// }
 
 //call via then/catch
 
@@ -132,3 +132,14 @@ async function maslahatBering(a) {
 //   }
 // });
 
+//mit task 2
+
+function countNumber(inputString) {
+  const digits = inputString.match(/\d/g);
+
+  return digits ? digits.length : 0;
+}
+
+const input = "Hello123World456!";
+const numberCount = countNumber(input);
+console.log(`The string contains ${numberCount} numbers.`);
